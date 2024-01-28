aa218dc68da530d61d5b225905720e6e29f6b4e5
NayaneRosa <nayaneksr95@gmail.com>
2024-01-28 20:20:44 -0300

    CRUD de Temas

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,9 @@ import Login from './paginas/home/login/Login';
 import Cadastro from './cadastro/Cadastro';
 import Home from './paginas/home/Home';
 import { AuthProvider } from './contextos/AuthContext';
+import ListaTemas from './componentes/temas/listaTemas/ListaTemas';
+import FormularioTema from './componentes/temas/formularioTema/FormularioTema';
+import DeletarTema from './componentes/temas/deletarTema/DeletarTema';
 
 
 function App() {
@@ -21,6 +24,10 @@ function App() {
               <Route path="/login" element={<Login />} />
               <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/home" element={<Home />} />
+              <Route path="/temas" element={<ListaTemas />} />
+              <Route path="/cadastroTema" element={<FormularioTema />} />
+              <Route path="/editarTema/:id" element={<FormularioTema />} />
+              <Route path="/deletarTema/:id" element={<DeletarTema />} />
             </Routes>
           </div>
           <Footer />
