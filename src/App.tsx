6357c5fff0374b23e261cf20e71a7b4bfdbc5701
NayaneRosa <nayaneksr95@gmail.com>
2024-01-28 17:26:57 -0300

    criando sistema rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
 import './App.css';
 import Home from './paginas/home/Home';
+import Navbar from './componentes/navBar/NavBar';
+import Footer from './componentes/footer/Footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './paginas/home/login/Login';
+
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/home" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
+    <BrowserRouter>
+        <Navbar />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
     </>
-  );
+);
 }
 export default App;
