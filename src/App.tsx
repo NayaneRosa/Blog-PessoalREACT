707f4567d761d2b028a239df8c60f7b569e8db7b
NayaneRosa <nayaneksr95@gmail.com>
2024-01-28 15:52:01 -0300

    criando rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import Home from './paginas/home/Home';
 import './App.css';
-
+import Home from './paginas/home/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './paginas/home/login/Login';
 function App() {
- 
   return (
     <>
-      <Home/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
-
 export default App;
